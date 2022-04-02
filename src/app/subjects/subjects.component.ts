ba5d27ac2338e0c86f2a6b8ddcbb44d5af25f8e8
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-04-01 21:10:13 -0300

    1 file modified

--- src/app/subjects/subjects.component.ts
+++ src/app/subjects/subjects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SubjectsService } from '../subjects.service';
 
 @Component({
   selector: 'app-subjects',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public subjects: SubjectsService) { }
 
   ngOnInit() {
   }
